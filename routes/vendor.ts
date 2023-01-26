99d4a238bee1f8f3817b2f162b7e13880d256312
Akash Raju M <akashrajum7@gmail.com>
2023-01-26 16:39:00 +0530

    add vendor apis to update multiple apparels

--- routes/vendor.ts
+++ routes/vendor.ts
@@ -84,4 +84,84 @@ routes.patch(
   }
 );
 
+routes.patch(
+  "/apparel",
+  body("apparel").isArray(),
+  body("apparel.*.code").isString(),
+  body("apparel.*.size").isString(),
+  body("apparel.*.quality")
+    .custom(
+      (quality) => typeof quality === "number" && quality >= 0 && quality <= 5
+    )
+    .optional(),
+  body("apparel.*.price")
+    .custom((price) => typeof price === "number" && price >= 0)
+    .optional(),
+  body("apparel.*.stock")
+    .custom((stock) => typeof stock === "number" && stock >= 0)
+    .optional(),
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const apparel: Apparel[] = req.body.apparel;
+
+    // Read the apparel.json file and search for the apparel code and size. If found, update the quality and price. If not found, return an error message.
+    fs.readFile("apparel.json", "utf-8", (err: any, data: any) => {
+      if (err) {
+        return res.status(500).json({ message: "Server error" });
+      }
+
+      const apparelList: Apparel[] = JSON.parse(data);
+
+      let error: boolean = false;
+
+      const updatedApparel: Apparel[] = apparel.map((apparel: Apparel) => {
+        const apparelIndex = apparelList.findIndex(
+          (apparelList: Apparel) =>
+            apparelList.code === apparel?.code &&
+            apparelList.size === apparel?.size
+        );
+
+        if (apparelIndex === -1) {
+          error = true;
+          return apparelList[apparelIndex];
+        }
+
+        if (apparel?.quality) {
+          apparelList[apparelIndex].quality = apparel.quality;
+        }
+
+        if (apparel?.price) {
+          apparelList[apparelIndex].price = apparel.price;
+        }
+
+        if (apparel?.stock) {
+          apparelList[apparelIndex].stock = apparel.stock;
+        }
+
+        return apparelList[apparelIndex] as Apparel;
+      });
+
+      if (error) {
+        return res.status(404).json({ message: "Apparel not found" });
+      }
+
+      // Write the updated apparel.json file and return the updated stock.
+      fs.writeFile(
+        "apparel.json",
+        JSON.stringify(updatedApparel, null, 2),
+        (err: any) => {
+          if (err) {
+            return res.status(500).json({ message: "Server error" });
+          }
+          return res.status(200).json(updatedApparel);
+        }
+      );
+    });
+  }
+);
+
 export default routes;
